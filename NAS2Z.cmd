d9c03eb52c18cdd2b60ebcc86ae74ea14b194612
August-Icekimo <august.icekimo@gmail.com>
2020-04-30 17:03:02 +0800

    更新中間訊息

--- NAS2Z.cmd
+++ NAS2Z.cmd
@@ -3,5 +3,5 @@ ECHO Delete Z: link First
 net use * /d /YES
 ECHO Linking to Z: again...
 NET USE Z: \\192.168.1.139\文件 /user:icekimo PahoIsi
-ECHO Done !
+ECHO Done ! Now push new files to NAS
 WSL -u root /home/icekimo/syncup.sh
