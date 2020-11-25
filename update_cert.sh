03eb25d4944d7d6df1df3978d5e56afb151071c0
August Icekimo <august.icekimo@gmail.com>
2020-11-25 12:49:38 +0800

    更多nginx停止的嘗試

--- update_cert.sh
+++ update_cert.sh
@@ -2,7 +2,7 @@
 #!/bin/sh
 acmePath="/home/icekimo/.acme.sh"
 certFQDN="jenkins.icekimo.idv.tw"
-service nginx stop
+service nginx stop || nginx -s stop
 echo " Nginx Stoped, now update ssl cert: $certFQDN"
 $acmePath/acme.sh --config-home /etc/acme --force --debug --issue --standalone  -d $certFQDN
 service nginx start
