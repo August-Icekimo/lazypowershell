2ba0f6a1ca0ebff1b3dfe5b7f97618860e896a33
August Icekimo <august.icekimo@gmail.com>
2021-02-10 14:56:21 +0800

    使用Oracle JRE才會動

--- BOIC/upload.sh
+++ BOIC/upload.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-/usr/bin/javaws ./launch.jnlp
+# oracle jre ref URL 
+# https://sdlc-esd.oracle.com/ESD6/JSCDL/jdk/8u281-b09/89d678f2be164786b292527658ca1605/jre-8u281-linux-x64.tar.gz?GroupName=JSC&FilePath=/ESD6/JSCDL/jdk/8u281-b09/89d678f2be164786b292527658ca1605/jre-8u281-linux-x64.tar.gz&BHost=javadl.sun.com&File=jre-8u281-linux-x64.tar.gz&AuthParam=1612939773_38e920070174b543f23d3047da6bef43&ext=.gz
+export JAVAHOME="/usr/bin/jre1.8.0_281"
+export PATH="$PATH;$JAVAHOME/bin"
+/usr/bin/jre1.8.0_281/bin/javaws ./launch.jnlp
