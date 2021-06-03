2498e279e7aa754b552489bdedb76f88f7e49de5
August Icekimo <august.icekimo@gmail.com>
2021-06-03 10:42:32 +0800

    修改為可安裝也可重複啟動服務

--- install_jenkins_docker.sh
+++ install_jenkins_docker.sh
@@ -4,23 +4,39 @@ export my_nvme="/srv/nvme/jenkins"
 export DOCKER_TLS_CERTDIR_PATH="$my_nvme/certs"
 export jenkinsCertsVol="$my_nvme/certs/client"
 export jenkinsDataVol="$my_nvme/var/jenkins_home"
-mkdir -p $DOCKER_TLS_CERTDIR_PATH
-mkdir -p $jenkinsCertsVol
-mkdir -p $jenkinsDataVol
+# Create folders for the very first day.
+if [ -d $my_nvme ];
+then 
+  echo "Step 0.1: Jenkins Requried Folder Ready"
+else
+  mkdir -p $DOCKER_TLS_CERTDIR_PATH
+  mkdir -p $jenkinsCertsVol
+  mkdir -p $jenkinsDataVol
+fi
+
 # Create a bridge network in Docker named "jenkins"
-docker network create jenkins
+if [ $(docker network ls | grep jenkins | wc -l ) -gt 0 ];
+then 
+  echo "Step 0.2: Jenkins Network Ready"
+else
+  docker network create jenkins
+fi
 
 # Run the jenkins docker for first step
+echo "Step 1: Start Jenkins Docker IN Docker."
 docker run --name jenkins-docker --rm --detach \
   --privileged --network jenkins --network-alias docker \
   --env DOCKER_TLS_CERTDIR=$DOCKER_TLS_CERTDIR_PATH \
   --volume jenkins-docker-certs:$jenkinsCertsVol \
   --volume jenkins-data:$jenkinsDataVol \
   --publish 2376:2376 docker:dind
+# Change jenkins-docker container to non-stop service
+docker update --restart unless-stopped jenkins-docker
 
 # Homebrew jenkins LTS + blueocean
 # https://hub.docker.com/repository/docker/icekimo/jenkins/general
 # No autobuild , due to dockerhub "一夜兩次郎" polocy.
+echo "Step 2: Start Jenkins BlueOcean."
 docker run --name jenkins-blueocean --rm --detach \
   --network jenkins --env DOCKER_HOST=tcp://docker:2376 \
   --env DOCKER_CERT_PATH=$DOCKER_TLS_CERTDIR_PATH --env DOCKER_TLS_VERIFY=1 \
@@ -31,6 +47,3 @@ docker run --name jenkins-blueocean --rm --detach \
 
 # Post Install 
 # https://www.jenkins.io/doc/book/installing/docker/#setup-wizard
-
-# Change jenkins-docker container to non-stop service
-docker update --restart unless-stopped jenkins-docker
