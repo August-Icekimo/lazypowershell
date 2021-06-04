fc31203128ab89f044409f99993346d2b2162647
August Icekimo <august.icekimo@gmail.com>
2021-06-04 10:31:38 +0800

    正式堪用的版本

--- rsyncData.sh
+++ rsyncData.sh
@@ -2,10 +2,9 @@
 # 變數宣告
 export fileslist=""
 export pathShift=""
-reverseDir=""
+_reverseDir=""
 _debug=""
 _dryrun=""
-
 
 # 前段函式宣告
 _printHelp()
@@ -13,18 +12,20 @@ _printHelp()
   #印出Debug說明或是幫助文字
   if [ "$1" == "help" ];
   then
-    echo ""
-    echo "使用-f 傳入目錄清單參數"
+    echo "使用-r | --reverse 轉換同步檔案的方向"
+    echo "使用-f | --folder 傳入目錄清單參數"
     # 用檔案控制要同步的目錄清單
     # 使用方式可為Cgwin / WSL 內 rsync
     # 須先建立目錄清單 flists.csv
-    echo "使用-p 同步目錄的位置參數"
+    echo "使用-p | --path 同步目錄的位置參數"
     # 同步目錄的位置參數
     # export pathShift="/mnt/d"
+    echo "使用-d | --debug 打開偵錯參數"
+    echo "使用--dry-run 僅進行暖身，不會真的執行"
   fi
   if [ "$1" == "debug" ];
   then
-  echo "反轉同步方向: $reverseDir";
+  echo "反轉同步方向: $_reverseDir";
   echo "同步目錄清單: $fileslist";
   echo "同步目錄位置: $pathShift";
   fi
@@ -35,7 +36,7 @@ while [ ${#} -gt 0 ];
   do 
     case ${1} in
     -r | --reverse )
-      reverseDir="TRUE"
+      _reverseDir="TRUE"
     ;;
     -f | --folder )
       fileslist="$2"
@@ -64,6 +65,7 @@ while [ ${#} -gt 0 ];
 
 _fileList()
 {
+  # 處理要不要用內建的目錄清單的函式
   if [ -z $1 ];
   then 
     if [ -r $(pwd)/flists.csv ];
@@ -83,7 +85,7 @@ _fileList()
 _fileList $fileslist
 
 # 偵錯用列印函式
-if [ -n $_debug ];
+if [ "$_debug" = "TRUE" ];
 then 
   _printHelp "debug"
 fi
@@ -91,17 +93,41 @@ fi
 
 # 目錄清單轉為單次指令稿
 scriptname="/tmp/runRsyncOnce$(date +%s).sh"
+touch $scriptname && chmod +x $scriptname
+echo "#!/bin/sh" > $scriptname
 
 # 逐行讀取轉入指令稿
-while IFS= read -r line || [[ "$line" ]];
-do
-    # echo "/usr/bin/rsync -zavuh --stats --progress $pathShift/$line $line $_dryrun" >> $scriptname
-    if [ -n $_debug ];
-    then
-      echo "新增同步目錄指令:"
-      echo "/usr/bin/rsync -zavuh --stats --progress \"$pathShift/$line\" \"$line\" $_dryrun"
-    fi
-done < $fileslist
-
-
-echo "----同步完成 Syncing Completed."
+if [ "$_reverseDir" = "TRUE" ];
+# 取得同步方向
+then
+  while IFS= read -r line || [[ "$line" ]];
+  do
+       echo "/usr/bin/rsync -zavuh --stats --progress \"$line\" \"$pathShift/$line\" $_dryrun" >> $scriptname
+      if [ "$_debug" = "TRUE" ];
+      then
+        echo "新增同步目錄指令:"
+        echo "/usr/bin/rsync -zavuh --stats --progress \"$line\" \"$pathShift/$line\" $_dryrun"
+      fi
+  done < $fileslist 
+else
+  while IFS= read -r line || [[ "$line" ]];
+  do
+        echo "/usr/bin/rsync -zavuh --stats --progress \"$pathShift/$line\" \"$line\ $_dryrun" >> $scriptname
+      if [ "$_debug" = "TRUE" ];
+      then
+        echo "新增同步目錄指令:"
+        echo "/usr/bin/rsync -zavuh --stats --progress \"$pathShift/$line\" \"$line\" $_dryrun"
+      fi
+  done < $fileslist
+fi
+# 開始正式執行腳本
+echo "----開始執行同步 RSyncing ....."
+sh $scriptname
+if  [ $? = 0 ] ; 
+then 
+  echo "----同步完成 RSync Completed."
+  rm $scriptname
+else
+  echo "----同步有問題，請檢查$scriptname ."
+  exit 1
+fi
