17dcaeab4fe475fe6b093dd5eecadccde5b5596a
August Icekimo <august.icekimo@gmail.com>
2021-07-03 13:42:10 +0800

    keep letsencrypt but Change-default-CA-to-ZeroSSL

--- update_cert.sh
+++ update_cert.sh
@@ -4,6 +4,6 @@ acmePath="/home/icekimo/.acme.sh"
 certFQDN="jenkins.icekimo.idv.tw"
 service nginx stop || nginx -s stop
 echo " Nginx Stoped, now update ssl cert: $certFQDN"
-$acmePath/acme.sh --config-home /etc/acme --force --debug --issue --standalone  -d $certFQDN
+$acmePath/acme.sh --config-home /etc/acme --force --debug --issue --standalone  -d $certFQDN --server letsencrypt
 service nginx start
 echo "Done."
