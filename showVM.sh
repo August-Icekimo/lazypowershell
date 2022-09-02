52e0ca6c30a78fc8745369eb756aa1691890e86c
August Icekimo <august.icekimo@gmail.com>
2022-09-02 11:05:13 +0800

    增加說明文字範本

--- showVM.sh
+++ showVM.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+# 
 VMName="$(basename "$(test -L "$0" && readlink "$0" || echo "$0")")"
 if [ "$VMName" = "showVM.sh" ]
 then
@@ -12,3 +13,25 @@ else
   echo "VM started."
 fi
 virt-viewer --full-screen $VMName
+
+: <<'END_OF_DOCS'
+=head1 NAME
+
+Show or start KVM Virtual Machine by vmname, RUN ln to change basename
+
+=head1 SYNOPSIS
+
+  Copy this shellscript to /usr/local/bin, and
+    ln -s /usr/local/bin/showVM.sh YourVMName
+  then, you can type "YourVMName" to connect to it.
+
+=head1 OPTIONS
+  No Options.
+
+=head1 DESCRIPTION
+
+=head1 LICENSE AND COPYRIGHT
+
+=cut
+
+END_OF_DOCS
