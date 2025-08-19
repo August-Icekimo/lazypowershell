9684a27046f840f3d76c17dec5e8393556fcf3a4
August Icekimo <August-Icekimo@users.noreply.github.com>
2025-08-19 09:07:18 +0800

    Update install_docker-ce.sh by official Doc

--- install_docker-ce.sh
+++ install_docker-ce.sh
@@ -1,11 +1,18 @@
 # install docker-ce
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common 
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
-sudo add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
-   $(lsb_release -cs) \
-   stable"
+ for pkg in docker.io docker-doc docker-compose docker-compose-v2 podman-docker containerd runc; do sudo apt-get remove $pkg; done
 
- sudo apt-get update && sudo apt-get install docker-ce docker-ce-cli containerd.io 
+ curl -fsSL https://get.docker.com -o get-docker.sh
+ sudo sh ./get-docker.sh --dry-run
+ sudo sh get-docker.sh
+
  sudo groupadd docker
  sudo usermod -aG docker $USER 
+
+ # Uninstall Docker Engine
+# sudo apt-get purge docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin docker-ce-rootless-extrasv
+# sudo rm -rf /var/lib/docker
+# sudo rm -rf /var/lib/containerd
+# sudo rm /etc/apt/sources.list.d/docker.list
+# sudo rm /etc/apt/keyrings/docker.asc
+
+# REF:https://docs.docker.com/engine/install/ubuntu/
